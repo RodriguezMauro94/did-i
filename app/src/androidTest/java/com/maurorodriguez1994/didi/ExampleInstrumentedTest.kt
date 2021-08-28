8691067413aaef5c641b78b725519e7e6d98150a
Mauro Rodriguez <mauro.rodriguez@mercadolibre.com>
2021-08-28 20:04:17 -0300

    added launcher and rename theme

--- app/src/androidTest/java/com/maurorodriguez1994/didi/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/maurorodriguez1994/didi/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.maurorodriguez1994.rooompractice", appContext.packageName)
+        assertEquals("com.maurorodriguez1994.didi", appContext.packageName)
     }
 }
